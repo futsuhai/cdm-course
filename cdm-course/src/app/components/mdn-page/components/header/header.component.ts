ae7a8cf957ab7157b35aa3fbde55a5bba2c5563c
rborisov <rborisov@codemastersintl.com>
2023-10-12 21:16:39 +0300

    added feedback

--- cdm-course/src/app/components/mdn-page/components/header/header.component.ts
+++ cdm-course/src/app/components/mdn-page/components/header/header.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  // host: {
+  //   class: 'header'  // Вот так мы аттачим класс header к компоненту app-header. После этого, в темплейте, мы можем спокойно создавать элементы этого блока
+  // }
 })
 export class HeaderComponent {
 
