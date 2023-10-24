c8abfccb5477e295098536c6d2be1cbb62e29557
futsuhai <130190351+futsuhai@users.noreply.github.com>
2023-10-24 14:15:41 +0300

    test

--- cdm-course/src/app/components/mdn-page/components/header/header.component.ts
+++ cdm-course/src/app/components/mdn-page/components/header/header.component.ts
@@ -4,10 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-   host: {
+  host: {
     class: 'header'
   }
 })
 export class HeaderComponent {
 
+  isBurgerIconActive = false;
+
+  toggleBurgerIcon() {
+    this.isBurgerIconActive = !this.isBurgerIconActive;
+  }
 }
